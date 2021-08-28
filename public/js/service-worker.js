c1bee081ef90edc37a42fd6816a01e268619f037
sdpercy <scottpercy@hotmail.com>
2021-08-28 17:37:22 -0230

    added service worker

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -64,9 +64,6 @@ self.addEventListener('fetch', function (e) {
       //   console.log(e.request.url)
         return fetch(e.request)
       }
-
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
     })
   )
 });
